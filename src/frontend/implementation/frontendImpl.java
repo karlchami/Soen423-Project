dcfae44ed4cbe709a7646dd6408d3a03c884cd0d
Karl Chami <karljchami@gmail.com>
2020-11-25 21:59:46 -0500

    Add response comparison + crash/failure notifications

--- src/frontend/implementation/frontendImpl.java
+++ src/frontend/implementation/frontendImpl.java
@@ -7,15 +7,12 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 import java.io.*;
 import java.rmi.AlreadyBoundException;
-import org.omg.CosNaming.*;
-import org.omg.CosNaming.NamingContextPackage.*;
 import org.omg.CORBA.*;
-import org.omg.PortableServer.*;
-import org.omg.PortableServer.POA;
-import java.text.ParseException;
 
 import frontend.corba.frontendPOA;
 import frontend.utils.Tuple;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import frontend.utils.RequestBuilder;
 
 public class frontendImpl extends frontendPOA  {
@@ -119,7 +116,7 @@ public class frontendImpl extends frontendPOA  {
 		
 	public String returnItem (String customerID, String itemID, String dateOfReturn) {	
 		String request = RequestBuilder.returnItemRequest(customerID, itemID, dateOfReturn);
-		String message = messageBuild(address, port, request);sendRequest(message, sequencer_address, sequencer_port);
+		String message = messageBuild(address, port, request);
 		sendRequest(message, sequencer_address, sequencer_port);
 		try {
 			String response = recieve(socket);
@@ -153,8 +150,8 @@ public class frontendImpl extends frontendPOA  {
 		}
 	}
 		
-    private static String messageBuild(InetAddress address, int port, String arguments) {
-		return address.toString() + ";" + Integer.toString(port) + ";" + arguments;
+    private static String messageBuild(InetAddress address, int port, String request) {
+		return address.toString() + ";" + Integer.toString(port) + ";" + request;
 	}
     
     public static void sendRequest(String message, InetAddress inet_address, int port) {
@@ -170,9 +167,9 @@ public class frontendImpl extends frontendPOA  {
                     byte[] buffer = new byte[1000];
                     DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
                     sendSocket.receive(reply);
-                    String response = new String(reply.getData());
+                    String response = new String(reply.getData()).trim();
                     // TODO: Agree on a reply message that denotes a received request
-                    if (response != null) {
+                    if (response.equals("received")) {
                         received = true;
                     }
                 } catch (SocketTimeoutException e) {
@@ -188,7 +185,7 @@ public class frontendImpl extends frontendPOA  {
     	String response_message;
     	// Expected number of replies (RM info tuple size)
     	int reply_count = rm_info.size();  	
-    	// Holds info about RMs that send UDP back to FE
+    	// Holds info about RMs that send UDP back to FE, used for tracking responses
     	ArrayList<Tuple<InetAddress, Integer, String>> received_rm = new ArrayList<Tuple<InetAddress, Integer, String>>(); 
     	// Determines whether we should keep receiving responses or not
     	Boolean receive = true;
@@ -216,25 +213,103 @@ public class frontendImpl extends frontendPOA  {
     		return response_message;
     		
     	} catch (SocketException ex) {
-            System.out.println("Error connecting to port");
-            System.exit(1);
+    		// Should make sure we get a response
+    		response_message = compareResponses(received_rm);
+    		return response_message;
         } catch (IOException ex) {
         	throw ex;
         }
-		return "No response received";
     }
     
-    // Compares JSON responses from different RMs
+    public String[] decodeResponse(String JSONString){
+    	// [replica_id, sequence_id, response_details]
+    	String[] decoded_response = new String[3];
+        try {
+            JSONParser jp = new JSONParser();
+            JSONObject JObject = (JSONObject) jp.parse(JSONString);
+            decoded_response[0] = JObject.get("response_details").toString();
+            decoded_response[1] = JObject.get("sequence_id").toString();
+            decoded_response[2] = JObject.get("replica_id").toString();
+        }catch (Exception e) {
+        	e.printStackTrace();
+        }
+        return decoded_response;
+    }
+    
+    public String[] decodeResponseDetails(String JSONString) {
+    	// [status_code, message]
+    	String[] decoded_details = new String[2];
+    	try {
+    		JSONParser jp = new JSONParser();
+    		JSONObject JObject = (JSONObject) jp.parse(JSONString);
+    		decoded_details[0] = JObject.get("status_code").toString();
+    		decoded_details[1] = JObject.get("message").toString();
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return decoded_details;
+    }
+    
+    // Compares JSON responses from different RMs and returns the 
     public String compareResponses(ArrayList<Tuple<InetAddress, Integer, String>> received_rm) {
-    	// TODO: Decode json string, get needed comparison params and construct them into a string for comparison
+    	String response_message = null;
+    	int correct_responses = 0;
+    	
+    	// To get the response message majority
+    	for(Tuple<InetAddress, Integer, String> single_rm : received_rm) {
+    		// getName is the equivalent of getResponseMessage
+    		String raw_response = single_rm.getName().trim();
+    		String[] decoded_response = this.decodeResponse(raw_response);
+    		
+    		// Get all response information needed
+    		String replica_id = decoded_response[0];
+    		String sequence_id = decoded_response[1];
+    		String[] response_details = this.decodeResponseDetails(decoded_response[2]);
+    		String status_code = response_details[0];
+    		String message = response_details[1];
+    		
+    		// Comparison logic
+    		if(correct_responses == 0) {
+    			response_message = message;
+    		} else if (response_message.equals(message)) {
+    			correct_responses++;
+    		} else {
+    			correct_responses--;    			
+    		}
+    		
+    		// Detect crashed RMs aka RMs that didn't reply
+    		if(received_rm.size() < correct_responses) {
+    			// Take a copy of RM arraylist
+    			ArrayList<Tuple<InetAddress, Integer, String>> crashed_rms = new ArrayList<Tuple<InetAddress, Integer, String>>(rm_info);
+    			// Remove all non-defective RMs from crashed list
+    			crashed_rms.removeAll(received_rm);
+    	    	for(Tuple<InetAddress, Integer, String> rm : crashed_rms) {
+    	    		notify_rm(rm.getInetAddress(), rm.getPort(), "CRASHED-RM");
+    	    	}
+    		}
+    		
+    		// To notify failed RMs aka RMs that had a wrong response message or failed status code
+    		for(Tuple<InetAddress, Integer, String> rm : received_rm) {
+        		// getName is the equivalent of getResponseMessage
+        		String raw = single_rm.getName().trim();
+        		String[] decoded = this.decodeResponse(raw);	
+        		String[] details = this.decodeResponseDetails(decoded[2]);
+        		String code = details[0];
+        		String msg = details[1];
+        		
+        		if( ( !response_message.equals(msg) ) | ( code.equals("Failed")) ) {
+    	    		notify_rm(rm.getInetAddress(), rm.getPort(), "FAILED-RM");
+        		}
+    		}
+    	}
+    	
 		return null;
     }
     
     // Notifies all RMs in multicast about a single RM failure
-    private void notify_rm(InetAddress address, int port) {
+    private void notify_rm(InetAddress address, int port, String status) {
     	for (Tuple<InetAddress, Integer, String> rm: rm_info) {
-    		String message = "FAILED-RM" + "," + address + "," + port;
-    		String response = messageBuild(address, port, message);
+    		String message = status + "," + address + "," + port;
     		new Thread(() -> {
     			sendRequest(message, rm.getInetAddress(), rm.getPort());
             }).start();
